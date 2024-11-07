eb074cea38f42093190ae07bfdaee3d22bc00fe7
zahoorshinwari <khanzshinwari5371@gmail.com>
2024-11-07 16:01:05 +0500

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+// index.js
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const { UserRouter } = require('./routes/user');
+const { AdminRouter } = require('./routes/admin');
+const { connectDB } = require('./config/db');
+const { CompanyRouter } = require('./routes/company');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+  origin: ["http://localhost:5173"], // Adjust as needed for your frontend
+  methods: ['POST', 'GET'],
+  credentials: true
+}));
+app.use(cookieParser());
+app.use('/api', UserRouter);
+
+app.use('/api/admin', AdminRouter); // Prefix the admin routes with /api/admin
+
+app.use('/api/', CompanyRouter)
+
+// Connect to the database and start the server
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on https://localhost:${process.env.PORT}`);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send("hello world")
+})
